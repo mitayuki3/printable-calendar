280c53431b2649db48bce4bbb31b268ea3777035
Yuki MITA <mitayuki3@yahoo.co.jp>
2026-01-04 16:00:26 +0900

    pnpm format

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import './App.css';
+import "./App.css";
 
 const App = () => {
-  return (
-    <div class="content">
-      <h1>Printable Calendar</h1>
-      <p>Welcome to your SolidJS calendar app!</p>
-    </div>
-  );
+	return (
+		<div class="content">
+			<h1>Printable Calendar</h1>
+			<p>Welcome to your SolidJS calendar app!</p>
+		</div>
+	);
 };
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
-import { render } from 'solid-js/web';
-import App from './App';
+import { render } from "solid-js/web";
+import App from "./App";
 
-const root = document.getElementById('root');
+const root = document.getElementById("root");
 
 if (root) {
-  render(() => <App />, root);
+	render(() => <App />, root);
 }
