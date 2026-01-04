19756d007c64fa5806372d93cdd9b0437757f324
Yuki MITA <mitayuki3@yahoo.co.jp>
2026-01-04 15:41:57 +0900

    静的ファイル出力

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,12 +1,18 @@
-import { defineConfig } from '@rsbuild/core';
-import { pluginBabel } from '@rsbuild/plugin-babel';
-import { pluginSolid } from '@rsbuild/plugin-solid';
+import { defineConfig } from "@rsbuild/core";
+import { pluginBabel } from "@rsbuild/plugin-babel";
+import { pluginSolid } from "@rsbuild/plugin-solid";
 
 export default defineConfig({
-  plugins: [
-    pluginBabel({
-      include: /\.(?:jsx|tsx)$/,
-    }),
-    pluginSolid(),
-  ],
+	plugins: [
+		pluginBabel({
+			include: /\.(?:jsx|tsx)$/,
+		}),
+		pluginSolid(),
+	],
+	output: {
+		target: "web",
+		distPath: {
+			root: "dist",
+		},
+	},
 });
